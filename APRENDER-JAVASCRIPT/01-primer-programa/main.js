e186098eba8feb76506ffc2c87e4a66eacdf1140
baek-tech <Joseluisyepesgiraldo@gmail.com>
2026-05-09 19:10:08 -0500

    Delete APRENDER-JAVASCRIPT directory

--- APRENDER-JAVASCRIPT/01-primer-programa/main.js
+++
@@ -1,7 +0,0 @@
-//alert("Hola que tal? soy un mensaje Bienvenido al curso");
-//alert("Estamos aprendiendo JS");
-
-console.log("javascript es el lenguage mas lindo");
-//console.log("soy un mensaje enviado desde JS");
-
-console.log(124 + 11);
--- APRENDER-JAVASCRIPT/03-constantes/main.js
+++
@@ -1,9 +0,0 @@
-// constante es una variable cuyo contenido jamas cambia.
-
-let nombre = "Yepes";
-const CC = 1107034933;
-
-nombre = "Dominick";
-CC = 491177710;
-
-console.log(nombre, CC);
